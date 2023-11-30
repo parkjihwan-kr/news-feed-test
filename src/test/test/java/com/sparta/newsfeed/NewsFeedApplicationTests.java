b6b135d71e3506d60609e778dc4132092cfeb6bc
mankcom3799@hufs.ac.kr <mankcom3799@hufs.ac.krecho>
2023-11-30 18:06:46 +0900

    test #1 협업한 것에 영향이 가서 새로운 래포지토리 이동

--- src/test/test/java/com/sparta/newsfeed/NewsFeedApplicationTests.java
+++ src/test/test/java/com/sparta/newsfeed/NewsFeedApplicationTests.java
@@ -1,13 +1,14 @@
-package com.pjh.newsfeedtest;
+package com.sparta.newsfeed;
 
+
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@RequiredArgsConstructor
 @SpringBootTest
-class NewsFeedTestApplicationTests {
-
+class NewsFeedApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
